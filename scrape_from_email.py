d1a4666873f93a98e35dec7a031579d1f29b0108
Dustin Hladek <dwhladek@protonmail.com>
2019-02-07 15:37:52 -0800

    updated
    end to end scraping from email to dataframe that matches the database in google cloud

--- scrape_from_email.py
+++ scrape_from_email.py
@@ -14,13 +14,14 @@ import matplotlib.pyplot as plt
 import datetime 
 from datetime import date, timedelta
 from bs4 import BeautifulSoup
+import datetime as dt
 
 # connect to gmail imap server
 m = imaplib.IMAP4_SSL('imap.gmail.com')
 # fill in username
-username = '#####'
+username = '#######'
 # fill in password
-password = '#####'
+password = '######'
 # login
 m.login(username, password)
 
@@ -73,19 +74,110 @@ prop_dict={}
 prop_dict['data']=[]
 for i in range(len(alls)):
   prop_dict['data'].append({
-      'address': alls[i][0],
+      'full_address': alls[i][0],
       'display_price': alls[i][1], 
       'listing_id': alls[i][3],
       'status': alls[i][5],
-      'sqft' : alls[i][7],
+      'sqft_all' : alls[i][7],
       'bed': alls[i][9],
       'bath': alls[i][10],
       'pool': alls[i][12],
       'view': alls[i][14],
       'yrbuilt': alls[i][16],
       'type': alls[i][18],
-      'contract_status_change_date': df_date
+      'contract_status_change_date': df_date, 
+      'state':'CA'
   })
 
-props = pd.DataFrame(prop_dict['data'],columns=['address', 'display_price', 'listing_id', 'status', 'sqft', 'bed', 'bath', 'pool', 'view', 'yrbuilt', 'type', 'contract_status_change_date'])
-props
+props = pd.DataFrame(prop_dict['data'],columns=['full_address', 'display_price', 'listing_id', 'status', 'sqft_all', 'bed', 'bath', 'pool', 'view', 'yrbuilt', 'type', 'contract_status_change_date', 'state'])
+
+
+
+#strip whitepspace
+props1 = props1.applymap(lambda x: x.strip())
+# creating streetname and city columns from address
+props1[['address', 'city']]= pd.DataFrame(props1.full_address.str.split(',').tolist()).iloc[:, 0:2]
+# getting sqft(first part of sqft_all)
+props1['sqft'] = pd.DataFrame(props1.sqft_all.str.split().tolist()).iloc[:,0]
+# attached or detached?
+props1['a/d'] = np.where(props1.type =='SFR', 'D', 'A')
+props1.head()
+
+#getting geocode_address so that I can call google api for lat and lon
+props1['st#'] = pd.DataFrame(props1['address'].str.split().tolist()).iloc[:,0]
+props1['st_name_only'] = props1['address'].str.replace('\d+', '').str.replace('\#','')
+geocode_cols = list(['st#', 'st_name_only','city','state'])
+props1 = props1.applymap(lambda x: x.strip())
+props1['geocode_address'] = props1[geocode_cols].apply(lambda x: '+'.join(x.astype(str)), axis=1)
+
+# code good for now. dont want to repeat it
+import requests
+#url initialized
+url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+# my PRIVATE api key
+api = '#########'
+#column to iterate over
+col = props1['geocode_address']
+# dictionary to store lats and lons
+geo_data = {}
+# making appending easier to create a list inside the dict
+geo_data['loc'] =[]
+# loop over the length of the column
+for i in range(len(col)):
+    # using the requests library. we are getting the data from the url for each address in column
+    results = requests.get(url + str(props1['geocode_address'][i]) + api)
+    # taking the data and making it a json
+    json_data = json.loads(results.text)
+    # appending lat and lon into the dictionary
+    try:
+        geo_data['loc'].append({
+            # get the latitude
+            'lat': json_data['results'][0]['geometry']['location']['lat'],
+            # get the longitude
+            'lon' : json_data['results'][0]['geometry']['location']['lng']
+    })
+    except:
+        geo_data['loc'].append({
+            # fill lat with 0 if error occurs
+            'lat': 0,
+            # fill lon with 0 if error occurs
+            'lon' : 0
+    })
+
+loc_data = pd.DataFrame(geo_data['loc'], columns=['lat', 'lon'])
+props1 = props1.join(loc_data)
+
+props1['price'] = pd.to_numeric(props['display_price'].str.replace('$', '').str.replace(',',''))
+props1['price_per_sqft'] = round(props1['price'] / pd.to_numeric(props1['sqft'].str.replace(',','')),2)
+#want the display price so we can present it as a currency
+props1['display_price_per_sqft'] = props1['price_per_sqft'].map("${:,.2f}".format)
+
+# checkpoint
+props2 = props1.copy()
+
+# turn contract status date into datetime so we can pull info out of it
+props2['contract_status_change_date'] = pd.to_datetime(props2.contract_status_change_date)
+props2['day_of_week'] = props2.contract_status_change_date.dt.weekday_name
+props2['month'] = [i.strftime('%b') for i in props2['contract_status_change_date']]
+props2['year'] = props2.contract_status_change_date.dt.year
+#change date format
+props2['contract_status_change_date']= props2['contract_status_change_date'].dt.strftime('%m/%d/%Y')
+
+# get rid of slash in bed
+props2['bed'] = props2.bed.str.replace('/', '')
+
+def to_bool(df, cols):
+  for col in cols:
+    df[col] = df[col].map({'Y':True, 'N':'False'})
+    
+to_bool(props2, ['pool', 'view'])
+
+props2.drop(['sqft_all', 'st#', 'st_name_only', 'geocode_address', 'full_address'], axis=1, inplace=True)
+
+# now we can send props2 to database
+
+# filler. need to figure out how to keep dom updated. its an important metric
+props2['dom'] = 1
+
+props2
+
